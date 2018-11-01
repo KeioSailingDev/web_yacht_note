09c593d2f588451ad80f66caaec18eb76c4ea683
Takafumi1224 <takafumisuzuki1224@gmail.com>
2018-11-01 21:26:41 +0900

    ①艇番-デバイス-スキッパー-クルーをボタンで表示できるように変更　②show_outline.htmlの表示機能を追加

--- static/js/outline.js
+++ static/js/outline.js
@@ -1,14 +1,31 @@
+// show_outline.html内の、練習メニューの表示
 $(function(){
 
   $('#menu > li:gt(0)').hide();
 
-  var Num = 0;
+  var Menu_Num = 0;
 
   $('#menu-open').click(function(){
 
-    Num += 1;
+    Menu_Num += 1;
 
-    $('#menu > li:lt('+Num+')').show();
+    $('#menu > li:lt('+Menu_Num+')').show();
+
+  });
+
+});
+
+//艇番-デバイス-スキッパー-クルーの一組を表示
+$(function(){
+  $('.yacht-skipper:gt(0)').hide();
+
+  var Yacht_Num = 0;
+
+  $('#open-yacht-skipper').click(function(){
+
+    Yacht_Num += 1;
+
+    $('.yacht-skipper:lt('+Yacht_Num+')').show();
 
   });
 
