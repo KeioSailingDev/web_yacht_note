ce57a799f9a809715b89b83953d113771cae06f6
Takafumi1224 <takafumisuzuki1224@gmail.com>
2018-12-10 21:48:12 +0900

    ①show_outline.htmlの練習メニューの入力欄で、「未入力」のもののみ、非表示にする

--- static/js/outline.js
+++ static/js/outline.js
@@ -1,15 +1,35 @@
 // show_outline.html内の、練習メニューの表示
 $(function(){
 
-  $('#menu > li:gt(0)').hide();
+  for(var list_num = 2; list_num < 16; list_num++){
 
-  var Menu_Num = 0;
+    //リストのインデックス番号を取得
+    var list_index = list_num - 2
+
+    //リストのidを生成
+    var list_id = 'training' + list_num;
+
+    //リストオブジェクトを取得
+    var target_list = document.getElementById(list_id);
+
+    //各リストで選択されている値を取得
+    var target_list_value = target_list.value;
+
+    //リストの値が「未入力」の場合は、以降のリストを非表示にする
+    if(target_list_value == ""){
+      $('#menu > li:gt('+list_index+')').hide();
+      var hidden_list = list_num
+      console.log(hidden_list)
+      break;
+    }
+
+  };
 
   $('#menu-open').click(function(){
 
-    Menu_Num += 1;
+    $('#menu > li:lt('+hidden_list+')').show();
 
-    $('#menu > li:lt('+Menu_Num+')').show();
+    hidden_list += 1;
 
   });
 
