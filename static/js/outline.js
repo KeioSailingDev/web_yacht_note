0c95dc0a60e8b28e7e763b3958b2ffab7c372dc9
鈴木貴文 <takafumisuzuki1224@gmail.com>
2019-03-24 17:05:56 +0900

    ①練習メニューの入力に、スタイダー追加（デザインのみ）

--- static/js/outline.js
+++ static/js/outline.js
@@ -53,3 +53,17 @@ $(function(){
   });
 
 });
+
+$(function(){
+  var elem = document.getElementsByClassName('time-range');
+  var rangeValue = function (elem, target) {
+　 　　return function(evt){
+　　　 target.innerHTML = elem.value;
+　　　  }
+　     }
+　    for(var i = 0, max = elem.length; i < max; i++){
+　　　  bar = elem[i].getElementsByTagName('input')[0];
+　　　  target = elem[i].getElementsByTagName('span')[0];
+　　　   bar.addEventListener('input', rangeValue(bar, target));
+　     }
+});
