649f73ca498fe0dac7f23f6dc2a20c6cdb237036
鈴木貴文 <takafumisuzuki1224@gmail.com>
2019-09-07 14:20:30 +0900

    google map apiを編集

--- static/js/gps_animation.js
+++ static/js/gps_animation.js
@@ -1,7 +1,42 @@
+// var map;
+// function initMap() {
+//   var latlng = {
+//     lat: 35.297058,
+//     lng: 139.5523478
+//   },
+//   map = new google.maps.Map(document.getElementById('map_canvas'), {
+//     center: latlng,
+//     zoom: 15
+//   });
+//   // 地図上にマーカー表示する
+//   marker = new google.maps.Marker({
+//     position: latlng,
+//     map: map
+// });
+// }
+
+
+
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map_canvas'), {
-    center: {lat: 35.284470, lng: 139.565830},
+
+  // MTMLから、地図を表示したいcanvas要素を取得する
+  var canvas = document.getElementById('map_canvas');
+
+  var latlng = {
+    lat: 35.297058,
+    lng: 139.5523478
+  };
+
+  var map_options = {
+    center: latlng,
     zoom: 15
+  }
+
+  map = new google.maps.Map(canvas,map_options);
+  // 地図上にマーカー表示する
+  marker = new google.maps.Marker({
+    position: latlng,
+    map: map
   });
 }
