81e7e86362074af7f911a88cdc500da11d2660a7
鈴木貴文 <takafumisuzuki1224@gmail.com>
2019-09-07 15:48:50 +0900

    マーカーを表示

--- static/js/gps_animation.js
+++ static/js/gps_animation.js
@@ -1,21 +1,3 @@
-// var map;
-// function initMap() {
-//   var latlng = {
-//     lat: 35.297058,
-//     lng: 139.5523478
-//   },
-//   map = new google.maps.Map(document.getElementById('map_canvas'), {
-//     center: latlng,
-//     zoom: 15
-//   });
-//   // 地図上にマーカー表示する
-//   marker = new google.maps.Marker({
-//     position: latlng,
-//     map: map
-// });
-// }
-
-
 
 var map;
 function initMap() {
@@ -23,18 +5,22 @@ function initMap() {
   // MTMLから、地図を表示したいcanvas要素を取得する
   var canvas = document.getElementById('map_canvas');
 
+  // map中心の座標
   var latlng = {
     lat: 35.297058,
     lng: 139.5523478
   };
 
+  // 中心地点の指定と、マップの拡大具合
   var map_options = {
     center: latlng,
     zoom: 15
   }
 
+  // goole mapを表示
   map = new google.maps.Map(canvas,map_options);
-  // 地図上にマーカー表示する
+
+  // マーカー表示する
   marker = new google.maps.Marker({
     position: latlng,
     map: map
