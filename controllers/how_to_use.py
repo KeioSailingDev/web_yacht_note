226333418f242a9b5a4c5996a2a1a5059b81cdc0
fujit33 <thisis.snsd03@gmail.com>
2019-09-16 16:05:06 +0900

    キャッシュを整理

--- controllers/how_to_use.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-# "how_to_use_c"という名前でBlueprintオブジェクトを生成します
-how_to_use_c = Blueprint('how_to_use_c', __name__)
-
-@how_to_use_c.route("/how_to_use")
-def how_to_use():
-    """使い方ページ"""
-    return render_template('how_to_use.html')
